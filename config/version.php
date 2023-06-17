d4df182254d359c21dbcfe561384fee7eb399f65
amitspn <amit.tiwari@risertechub.in>
2023-06-17 11:13:44 +0530

    first commit

---
+++ config/version.php
@@ -0,0 +1,11 @@
+<?php
+return array (
+  'app_version' => 'v6.1.1-pre',
+  'full_app_version' => 'v6.1.1-pre - build 10653-g11cd875c6',
+  'build_version' => '10653',
+  'prerelease_version' => '',
+  'hash_version' => 'g11cd875c6',
+  'full_hash' => 'v6.1.1-pre-411-g11cd875c6',
+  'branch' => 'master',
+);
+
